72064e1674869e6012180b30f75a6d29270e15fd
Jean-Brice Maisner <jbmaisner@enneade.net>
2018-04-05 16:58:59 +0200

    compatibilité IE de merde

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		'postal-codes': __dirname + '/postal-codes.js',
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.json$/,
+				loader: 'json-loader',
+				type: 'javascript/auto',
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+};
+
+if (process.env.NODE_ENV === 'production') {
+	module.exports.mode = 'production';
+	module.exports.devtool = '#source-map';
+	// http://vue-loader.vuejs.org/en/workflow/production.html
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"',
+			},
+		}),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true,
+		}),
+	]);
+}
